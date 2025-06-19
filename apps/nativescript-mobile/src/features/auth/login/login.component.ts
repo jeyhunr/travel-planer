46a94b3ce145fc017b7bb2768e72781f3bb3b9e9
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2025-06-19 15:10:17 +0200

    add: login screen layout

--- apps/nativescript-mobile/src/features/auth/login/login.component.ts
+++ apps/nativescript-mobile/src/features/auth/login/login.component.ts
@@ -1,10 +1,43 @@
 import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
-import { NativeScriptCommonModule, NativeScriptRouterModule } from '@nativescript/angular';
+import { NativeScriptCommonModule, NativeScriptFormsModule, NativeScriptRouterModule } from '@nativescript/angular';
 
 @Component({
   selector: 'ns-login',
   templateUrl: './login.component.html',
-  imports: [NativeScriptCommonModule, NativeScriptRouterModule],
+  imports: [NativeScriptCommonModule, NativeScriptRouterModule, NativeScriptFormsModule],
   schemas: [NO_ERRORS_SCHEMA],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email = '';
+  password = '';
+  isPasswordVisible = false;
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  onLogin() {
+    if (this.validateForm()) {
+      console.log('Creating account with:', {
+        email: this.email,
+        password: this.password,
+      });
+    }
+  }
+
+  private validateForm(): boolean {
+    if (!this.email.trim()) {
+      alert('Bitte gib deine E-Mail-Adresse ein');
+      return false;
+    }
+    if (!this.password.trim()) {
+      alert('Bitte gib dein Passwort ein');
+      return false;
+    }
+    if (this.password.length < 6) {
+      alert('Das Passwort muss mindestens 6 Zeichen lang sein');
+      return false;
+    }
+    return true;
+  }
+}
