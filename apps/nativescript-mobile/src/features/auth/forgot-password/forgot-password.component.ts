a66b09152ecc66eecee9a561ba1a4765080d112c
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2025-06-19 15:11:07 +0200

    add: forgot-password screen layout

--- apps/nativescript-mobile/src/features/auth/forgot-password/forgot-password.component.ts
+++ apps/nativescript-mobile/src/features/auth/forgot-password/forgot-password.component.ts
@@ -1,10 +1,25 @@
 import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
-import { NativeScriptCommonModule, NativeScriptRouterModule } from '@nativescript/angular';
+import { NativeScriptCommonModule, NativeScriptFormsModule, NativeScriptRouterModule } from '@nativescript/angular';
 
 @Component({
   selector: 'ns-forgot-password',
   templateUrl: './forgot-password.component.html',
-  imports: [NativeScriptCommonModule, NativeScriptRouterModule],
+  imports: [NativeScriptCommonModule, NativeScriptRouterModule, NativeScriptFormsModule],
   schemas: [NO_ERRORS_SCHEMA],
 })
-export class ForgotPasswordComponent {}
+export class ForgotPasswordComponent {
+  email = '';
+  isLoading = false;
+
+  onForgotPassword() {
+    this.isLoading = true;
+
+    setTimeout(() => (this.isLoading = false), 2000);
+    // Implement password reset logic here
+    if (!this.email.trim()) {
+      alert('Bitte gib deine E-Mail-Adresse ein');
+      return;
+    }
+    alert('Password reset email sent successfully');
+  }
+}
