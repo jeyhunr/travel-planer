7af557209667c6dab19afe37b51d75a607e67f45
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2025-06-26 01:15:05 +0200

    fix: Dependency Injection on Interceotor

--- apps/nativescript-mobile/src/core/interceptors/http.interceptor.ts
+++ apps/nativescript-mobile/src/core/interceptors/http.interceptor.ts
@@ -1,14 +1,13 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AuthService } from '../services/auth.service';
+import { ApplicationSettings } from '@nativescript/core';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  private authService = inject(AuthService);
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.authService.token();
+    const token = ApplicationSettings.getString(environment.TOKEN_KEY, null);
 
     if (token) {
       const authReq = req.clone({
