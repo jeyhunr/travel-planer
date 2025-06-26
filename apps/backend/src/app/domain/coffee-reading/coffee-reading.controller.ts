1b5fb0e63ace188d7d2f4889d95fdb68c8ec203e
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2025-06-26 12:10:50 +0200

    add: coffee-readings share endpoint and update getReadings service function

--- apps/backend/src/app/domain/coffee-reading/coffee-reading.controller.ts
+++ apps/backend/src/app/domain/coffee-reading/coffee-reading.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { CoffeeReadingService } from './coffee-reading.service';
+import { JwtAuthGuard } from '../../common/guard';
 
 @Controller('coffee-readings')
 export class CoffeeReadingController {
   constructor(private coffeeReadingService: CoffeeReadingService) {}
 
   @Get('all/:skip/:take')
+  @UseGuards(JwtAuthGuard)
   async getReadings(@Param('skip') skip: string, @Param('take') take: string) {
     return await this.coffeeReadingService.getReadings(parseInt(skip), parseInt(take));
+  }
+
+  @Get('my/:skip/:take')
+  @UseGuards(JwtAuthGuard)
+  async getMyReadings(@Param('skip') skip: string, @Param('take') take: string, @Req() { headers }) {
+    return await this.coffeeReadingService.getReadings(parseInt(skip), parseInt(take), headers.authorization);
+  }
+
+  @Put('share/:uid')
+  @UseGuards(JwtAuthGuard)
+  async shareReading(@Param('uid') uid: string, @Req() { headers }) {
+    return await this.coffeeReadingService.shareReading(uid, headers.authorization);
   }
 }
--- apps/backend/src/app/domain/coffee-reading/coffee-reading.service.ts
+++ apps/backend/src/app/domain/coffee-reading/coffee-reading.service.ts
@@ -1,13 +1,31 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '@travel-planer/prisma-client';
 
 @Injectable()
 export class CoffeeReadingService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService
+  ) {}
 
-  getReadings(skip: number, take: number) {
+  async getReadings(skip: number, take: number, token?: string) {
     try {
-      return this.prisma.coffeeReading.findMany({
+      let where: any = {};
+
+      if (token) {
+        const decodedToken = this.jwtService.decode(token.split(' ')[1]) as { email: string };
+        const email = decodedToken.email;
+
+        const user = await this.prisma.user.findUnique({ where: { email } });
+        if (user) {
+          where = { ...where, userUid: user.uid };
+        }
+      } else {
+        where = { ...where, isShared: true };
+      }
+
+      const posts = await this.prisma.coffeeReading.findMany({
         select: {
           uid: true,
           overallInterpretation: true,
@@ -20,12 +38,39 @@ export class CoffeeReadingService {
         orderBy: {
           createdAt: 'desc',
         },
-        where: {
-          isShared: true,
-        },
+        where,
       });
+
+      return posts.map((x) => ({
+        uid: x.uid,
+        overallInterpretation: x.overallInterpretation,
+        user: x.User.username,
+        imageUrl: x.imageUrl,
+        createdAt: x.createdAt,
+      }));
     } catch (error) {
       throw new InternalServerErrorException('Error getting coffee readings');
+    }
+  }
+
+  async shareReading(uid: string, token: string) {
+    try {
+      const decodedToken = this.jwtService.decode(token.split(' ')[1]) as { email: string };
+      const email = decodedToken.email;
+
+      const user = await this.prisma.user.findUnique({ where: { email } });
+      if (user) {
+        await this.prisma.coffeeReading.update({
+          where: { uid, userUid: user.uid },
+          data: { isShared: true },
+        });
+      } else {
+        throw new InternalServerErrorException('User not found');
+      }
+
+      return { message: 'Coffee reading shared successfully.' };
+    } catch (error) {
+      throw new InternalServerErrorException('Error sharing coffee reading');
     }
   }
 }
